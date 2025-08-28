7d6f75d2c9d2d0e938aab343c71befd47dcd26a9
swemmanuelgz <zundercoc@gmail.com>
2025-08-28 23:50:56 +0200

    gateway funcionando con micro users

---
+++ src/main/java/com/viewnext/gateway/carrentalgateway/CarRentalGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.viewnext.gateway.carrentalgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CarRentalGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viewnext/gateway/carrentalgateway/CarRentalGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viewnext.gateway.carrentalgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
